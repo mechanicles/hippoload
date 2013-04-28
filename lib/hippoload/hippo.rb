d10b6fd04cada1bc25d8f5a463d256eb5c55e7c1
Santosh Wadghule <santosh.wadghule@gmail.com>
2013-04-28 21:53:28 +0530

    Hippo#attack with specs

--- lib/hippoload/hippo.rb
+++ lib/hippoload/hippo.rb
@@ -4,18 +4,32 @@ module Hippoload
     attr_reader :connections, :rate, :server, :port, :uri, :connections_and_rates
 
     def initialize(conf)
-
       @connections = conf[:connections]
       @rate = conf[:rate]
       @server = conf[:server] || 'localhost'
       @port = conf[:port] || '3000'
       @uri = conf[:uri]
-      @connections_and_rates = conf[:connections_and_rates] #|| default_connections_and_rates
+      @connections_and_rates = conf[:connections_and_rates] || default_connections_and_rates
 
       raise_error_if_wrong_conf
     end
 
+    def attack
+      %x(httperf --num-conns=#{connections} --rate=#{rate} --server=#{server} --port=#{port} --uri="#{uri}")
+    end
+
     private
+
+    def default_connections_and_rates
+      [
+        { :connections => 100,  :rate =>  10 },
+        { :connections => 200,  :rate =>  20 },
+        { :connections => 300,  :rate =>  30 },
+        { :connections => 500,  :rate =>  50 },
+        { :connections => 700,  :rate =>  70 },
+        { :connections => 1000, :rate => 100 }
+      ] if (!connections.nil? or !rate.nil?) && !connections_and_rates.nil?
+    end
 
     def raise_error_if_wrong_conf
       if (!connections.nil? || !rate.nil?) && !connections_and_rates.nil?
--- spec/hippo_spec.rb
+++ spec/hippo_spec.rb
@@ -3,33 +3,54 @@ require 'hippoload'
 describe Hippoload::Hippo do
 
   let(:basic_conf) { { :connections => 100, :rate => 10, :uri => "/posts" } }
-  let(:basic_conf) { { :connections => 100, :rate => 10, :uri => "/posts" } }
+  let(:advanced_conf) { { :uri => "/posts", :connections_and_rates => [{ :connections => 100, :rate => 10 }] } }
   let(:wrong_conf) do 
     {
       :connections => 100, 
       :rate => 10, 
       :uri => "/posts",
-      :connections_and_rates => [ { :connections => 100, :rate => 10 } ]
+      :connections_and_rates => [{ :connections => 100, :rate => 10 }]
     }
   end
 
-  it "should have attributes reader" do
-    hippo = Hippoload::Hippo.new(basic_conf)
-    hippo.connections.should eql(100)
-    hippo.rate.should eql(10)
-    hippo.server.should eql('localhost')
-    hippo.port.should eql('3000')
-    hippo.uri.should eql('/posts')
-    hippo.connections_and_rates.should nil
+  describe "Hippo#attributes_methods" do
+
+    it "should have attributes reader" do
+      hippo = Hippoload::Hippo.new(basic_conf)
+      hippo.connections.should eql(100)
+      hippo.rate.should eql(10)
+      hippo.server.should eql('localhost')
+      hippo.port.should eql('3000')
+      hippo.uri.should eql('/posts')
+      hippo.connections_and_rates.should nil
+    end
+
+    it "should raise error if wrong conf passed" do
+      expect { Hippoload::Hippo.new(wrong_conf) }.to raise_error
+    end
+
+    it "should not raise error if correct conf passed" do
+      expect { Hippoload::Hippo.new(basic_conf) }.to_not raise_error
+    end
+
+    it "should have connections_and_rates attribute if we passed advanced_conf" do
+      hippo = Hippoload::Hippo.new(advanced_conf)
+      hippo.connections_and_rates.should eql(advanced_conf[:connections_and_rates])
+    end
+
+    it "should have default connections and rates if passed advanced_conf without connections_and_rates attribute" do
+      advanced_conf.delete(:connections_and_rates)
+      hippo = Hippoload::Hippo.new(advanced_conf)
+      hippo.connections_and_rates.should eql(hippo.send(:default_connections_and_rates))
+    end
   end
 
-  it "should raise error if wrong conf passed" do
-    expect { Hippoload::Hippo.new(wrong_conf) }.to raise_error
+  describe "Hippo#attack" do
+
+    it "should return httperf raw output" do
+      hippo = Hippoload::Hippo.new(basic_conf)
+      hippo.attack.inspect.should include('httperf')
+    end
+
   end
-
-  it "should not raise error if correct conf passed" do
-    expect { Hippoload::Hippo.new(basic_conf) }.to_not raise_error
-  end
-
-
 end
